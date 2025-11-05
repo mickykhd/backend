8ac87a215f3b8c3e9ad0a62baca81fc43ecfcf4a
Ashrumochan Badajena <ashrumochan7@gmail.com>
2025-11-05 13:15:57 +0530

    Disable MySQL connection pool setup
    Comment out MySQL pool configuration and export.

--- database.js
+++ database.js
@@ -1,13 +1,14 @@
-import mysql from "mysql2/promise";
+// import mysql from "mysql2/promise";
 
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "admin",
-    database: "mydb",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+// const pool = mysql.createPool({
+//     host: "localhost",
+//     user: "root",
+//     password: "admin",
+//     database: "mydb",
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// });
 
-export default pool;
+// export default pool;
+
